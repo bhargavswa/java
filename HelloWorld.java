13abe41f2477baa07d24348b79de782672fe4e89
bhargavswa <47050816+bhargavswa@users.noreply.github.com>
2019-08-16 16:45:10 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,6 @@
 public class HelloWorld {
    public static void main(String[] args) {
-      // Prints "Hello, I am here in the class room.
+      // Prints "Hello, I am here in the room.
       System.out.println("Hell all, Our Leaders");
    }
 }
